97a5547cef1c27fcf21c811b11c632908a1d085f
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2022-12-28 17:44:35 +0100

    Add package author

---
+++ R/isopleuros-package.R
@@ -0,0 +1,30 @@
+#' @details
+#' \tabular{ll}{
+#'  **Package:** \tab isopleuros \cr
+#'  **Type:** \tab Package \cr
+#'  **Version:** \tab 0.0.0 \cr
+#'  **License:** \tab GPL-3 \cr
+#' }
+#'
+#' @author
+#' **Full list of authors and contributors** (alphabetic order)
+#'
+#' \tabular{ll}{
+#'  Nicolas Frerebeau \tab *Université Bordeaux Montaigne, France* \cr
+#' }
+#'
+#' **Package maintainer**
+#'
+#' Nicolas Frerebeau\cr
+#' \email{nicolas.frerebeau@@u-bordeaux-montaigne.fr}
+#'
+#' Archéosciences Bordeaux (UMR 6034)\cr
+#' Maison de l'Archéologie\cr
+#' Université Bordeaux Montaigne\cr
+#' 33607 Pessac cedex\cr
+#' France
+#' @name isopleuros-package
+#' @aliases isopleuros
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
